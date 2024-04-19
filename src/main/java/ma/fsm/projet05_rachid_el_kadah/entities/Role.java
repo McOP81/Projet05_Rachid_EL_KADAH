5754354ef139f1009844dca9f3e3470ca5f4a416
R-ELKADAH <rachidelkadah@email.com>
2024-04-19 19:52:34 +0100

    sql database

--- src/main/java/ma/fsm/projet05_rachid_el_kadah/entities/Role.java
+++ src/main/java/ma/fsm/projet05_rachid_el_kadah/entities/Role.java
@@ -1,5 +1,6 @@
 package ma.fsm.projet05_rachid_el_kadah.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,10 +15,12 @@ import java.util.List;
 public class Role {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "DESCRIPTION")
     private String desc;
     @Column(unique=true, length = 20)
     private String roleName;
     @ManyToMany(fetch = FetchType.EAGER)
     @ToString.Exclude
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<User> users = new ArrayList<>();
 }
---
+++ src/main/java/ma/fsm/projet05_rachid_el_kadah/web/UserController.java
@@ -0,0 +1,19 @@
+package ma.fsm.projet05_rachid_el_kadah.web;
+
+import ma.fsm.projet05_rachid_el_kadah.entities.User;
+import ma.fsm.projet05_rachid_el_kadah.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @GetMapping("/users/{username}")
+    public User user(@PathVariable String username){
+        User user1 =userService.findUserByUsername(username);
+        return user1;
+    }
+}
